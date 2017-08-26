9a792c30a5a7ce12435c38ac718248c6f82d44a8
Stephen Swatman <stephenswat@gmail.com>
2017-08-26 14:26:34 +0200

    Create models for EVE static data.

--- elmo/eve_sde/models.py
+++ elmo/eve_sde/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+    name = models.CharField(
+        db_index=True,
+        unique=True,
+        max_length=64
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Constellation(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+    region = models.ForeignKey(
+        Region,
+        related_name='constellations',
+        db_index=True)
+
+    name = models.CharField(
+        db_index=True,
+        unique=True,
+        max_length=64
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class SolarSystem(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+    constellation = models.ForeignKey(
+        Constellation,
+        related_name='systems',
+        db_index=True
+    )
+
+    name = models.CharField(
+        db_index=True,
+        unique=True,
+        max_length=64
+    )
+
+    security = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Planet(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+    system = models.ForeignKey(
+        SolarSystem,
+        related_name='planets',
+        db_index=True
+    )
+
+    number = models.IntegerField(db_index=True)
+
+
+class Moon(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+    planet = models.ForeignKey(
+        Planet,
+        related_name='moons',
+        db_index=True
+    )
+
+    number = models.IntegerField(db_index=True)
