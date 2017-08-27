44b2ae57aaa8af8bea701a299c6ec2fc271420d4
Stephen Swatman <stephenswat@gmail.com>
2017-08-27 19:35:00 +0200

    Add a landing page.

--- elmo/elmo/urls.py
+++ elmo/elmo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^/', TemplateView.as_view(template_name='landing.html')),
     url(r'^admin/', admin.site.urls),
     url(r'^moons/', include('moon_tracker.urls'))
 ]
