99682164461f7720a77e9b27f44dfd0737d5d57e
Stephen Swatman <stephenswat@gmail.com>
2017-08-28 15:27:45 +0200

    Annotate view queries to show total number of moons.

--- elmo/moon_tracker/views.py
+++ elmo/moon_tracker/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 
 from eve_sde.models import Region, Constellation, SolarSystem, Moon
 
 def list_universe(request):
-    regions = Region.objects.filter(id__lt=11000000)
+    regions = (
+        Region.objects
+        .filter(id__lt=11000000)
+        .annotate(num_moons=Count('constellations__systems__planets__moons'))
+    )
 
     return render(
         request,
@@ -18,7 +23,11 @@ def list_universe(request):
 
 def list_region(request, region):
     region_obj = get_object_or_404(Region, name=region)
-    constellations = Constellation.objects.filter(region=region_obj)
+    constellations = (
+        Constellation.objects
+        .filter(region=region_obj)
+        .annotate(num_moons=Count('systems__planets__moons'))
+    )
 
     return render(
         request,
@@ -32,7 +41,11 @@ def list_region(request, region):
 
 def list_constellation(request, constellation):
     constellation_obj = get_object_or_404(Constellation, name=constellation)
-    systems = SolarSystem.objects.filter(constellation=constellation_obj)
+    systems = (
+        SolarSystem.objects
+        .filter(constellation=constellation_obj)
+        .annotate(num_moons=Count('planets__moons'))
+    )
 
     return render(
         request,
