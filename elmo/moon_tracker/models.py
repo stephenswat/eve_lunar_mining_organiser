0d60fe02076d89399d2233710c39c51762574de1
Stephen Swatman <stephenswat@gmail.com>
2017-09-02 17:37:16 +0200

    Add models for keeping track of moon makeup.

--- elmo/moon_tracker/models.py
+++ elmo/moon_tracker/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+
+from eve_sde.models import Moon, Ore
 
 # Create your models here.
+class ScanResult(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='scans',
+        db_index=True
+    )
+
+    moon = models.ForeignKey(
+        Moon,
+        related_name='scans',
+        db_index=True
+    )
+
+class ScanResultOre(models.Model):
+    scan = models.ForeignKey(
+        ScanResult,
+        related_name='constituents',
+        db_index=True
+    )
+
+    ore = models.ForeignKey(
+        Ore,
+        related_name='+'
+    )
+
+    percentage = models.PositiveSmallIntegerField()
