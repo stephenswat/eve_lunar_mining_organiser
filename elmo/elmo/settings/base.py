51bfb544caf3589096b1d64e846fe00d888c65e7
Stephen Swatman <stephenswat@gmail.com>
2017-09-21 21:11:45 +0200

    Explicitly define auth pipeline in settings.

--- elmo/elmo/settings/base.py
+++ elmo/elmo/settings/base.py
@@ -43,6 +43,20 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.eveonline.EVEOnlineOAuth2',
 ]
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+
 ROOT_URLCONF = 'elmo.urls'
 
 TEMPLATES = [
