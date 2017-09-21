d812a79cff05a07c538c5284e46d02db44e8b651
Stephen Swatman <stephenswat@gmail.com>
2017-09-21 20:11:51 +0200

    Split settings into base and case-specific.

--- elmo/elmo/settings/base.py
+++ elmo/elmo/settings/base.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ogq_-@en(d1fyw0=qe-gk35_n^)rn%w(o=03g^wfkd*_qhh36^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -82,17 +69,6 @@ STATICFILES_DIRS = [
 ]
 
 WSGI_APPLICATION = 'elmo.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
---
+++ elmo/elmo/settings/local.template.py
@@ -0,0 +1,23 @@
+# TODO: Copy this file to 'local.py' and fill in values.
+
+import os
+
+from elmo.settings.base import *
+
+SECRET_KEY = 'YOUR KEY GOES HERE'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+SOCIAL_AUTH_EVEONLINE_KEY = ''
+SOCIAL_AUTH_EVEONLINE_SECRET = ''
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
+
+# None if all alliances can access this app, otherwise a list of ID's.
+VALID_ALLIANCE_IDS = None
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
