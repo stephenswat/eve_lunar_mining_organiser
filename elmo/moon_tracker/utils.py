e68e9bc397d760a0d16acc695a1815141ad3658c
Stephen Swatman <stephenswat@gmail.com>
2017-09-26 21:21:36 +0200

    Add helper functions for permissions.

---
+++ elmo/moon_tracker/utils.py
@@ -0,0 +1,22 @@
+def user_can_view_scans(user, moon):
+    return (
+        user_can_delete_scans(user, moon) or
+        user.has_perm('eve_sde.can_view_scans', moon.planet.system) or
+        user.has_perm('eve_sde.can_view_scans', moon.planet.system.constellation) or
+        user.has_perm('eve_sde.can_view_scans', moon.planet.system.constellation.region)
+    )
+
+def user_can_add_scans(user, moon):
+    return (
+        user_can_delete_scans(user, moon) or
+        user.has_perm('eve_sde.can_add_scans', moon.planet.system) or
+        user.has_perm('eve_sde.can_add_scans', moon.planet.system.constellation) or
+        user.has_perm('eve_sde.can_add_scans', moon.planet.system.constellation.region)
+    )
+
+def user_can_delete_scans(user, moon):
+    return (
+        user.has_perm('eve_sde.can_delete_scans', moon.planet.system) or
+        user.has_perm('eve_sde.can_delete_scans', moon.planet.system.constellation) or
+        user.has_perm('eve_sde.can_delete_scans', moon.planet.system.constellation.region)
+    )
