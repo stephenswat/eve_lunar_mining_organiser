2a8b4095c4fbf8902cac9ed6ca37743e31308341
Stephen Swatman <stephenswat@gmail.com>
2017-09-26 19:54:24 +0200

    Add an admin function to create permissions.

---
+++ elmo/moon_tracker/management/commands/create_permissions.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+class Command(BaseCommand):
+    help = 'Creates permissions on the EVE SDE for Django Guardian.'
+
+    def handle(self, *args, **options):
+        ct_region = ContentType.objects.get(app_label='eve_sde', model='region')
+        ct_constellation = ContentType.objects.get(app_label='eve_sde', model='constellation')
+        ct_solarsystem = ContentType.objects.get(app_label='eve_sde', model='solarsystem')
+
+        for ct in [ct_region, ct_constellation, ct_solarsystem]:
+            Permission.objects.create(
+                codename='can_view_scans',
+                name='Can view all scans in this location',
+                content_type=ct
+            )
+
+            Permission.objects.create(
+                codename='can_add_scans',
+                name='Can add new scans in this location',
+                content_type=ct
+            )
+
+            Permission.objects.create(
+                codename='can_delete_scans',
+                name='Can delete other people\'s scans in this location',
+                content_type=ct
+            )
