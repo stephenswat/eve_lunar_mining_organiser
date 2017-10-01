2960bc88653f3195bac30083c1fcd00e9a48c1bc
Stephen Swatman <stephenswat@gmail.com>
2017-10-01 17:51:44 +0200

    Add an admin layout for the scan results.

--- elmo/moon_tracker/admin.py
+++ elmo/moon_tracker/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from moon_tracker.models import ScanResult, ScanResultOre
 
-# Register your models here.
+
+class ScanResultOreInline(admin.TabularInline):
+    model = ScanResultOre
+
+
+class ScanResultAdmin(admin.ModelAdmin):
+    inlines = [
+        ScanResultOreInline,
+    ]
+
+    fieldsets = (
+        (None, {'fields': (
+            'owner',
+            'moon',
+        )}),
+    )
+
+    list_display = (
+        'moon',
+        'owner',
+    )
+
+    readonly_fields = (
+        'moon',
+        'owner',
+    )
+
+    search_fields = (
+        'moon',
+        'owner',
+    )
+
+    ordering = (
+        'moon',
+    )
+
+admin.site.register(ScanResult, ScanResultAdmin)
