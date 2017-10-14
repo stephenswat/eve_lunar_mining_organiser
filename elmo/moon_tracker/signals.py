21e832955cecc3e36aa83c36a3434a72575d8da7
Stephen Swatman <stephenswat@gmail.com>
2017-10-15 00:48:11 +0200

    Fix a mistake in the signals preventing migrations.

--- elmo/moon_tracker/signals.py
+++ elmo/moon_tracker/signals.py
@@ -4,9 +4,8 @@ from django.contrib.auth.models import Group
 
 from eve_auth.models import EveUser
 
-DEFAULT_GROUP = Group.objects.get(name='default')
 
 @receiver(post_save, sender=EveUser)
 def my_handler(sender, **kwargs):
     if sender.character_id != 0 and kwargs['created']:
-        DEFAULT_GROUP.user_set.add(kwargs['instance'])
+        Group.objects.get(name='default').user_set.add(kwargs['instance'])
