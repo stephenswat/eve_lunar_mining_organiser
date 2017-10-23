34279e29901d79f3f994f0de88c703cf78bdd82b
Stephen Swatman <stephenswat@gmail.com>
2017-10-23 16:26:05 +0200

    Style own and other scan results differently.

--- elmo/moon_tracker/templatetags/result_display.py
+++ elmo/moon_tracker/templatetags/result_display.py
@@ -5,5 +5,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('moon_tracker/scan_result.html')
-def display_scan(scan):
-    return {'scan': scan}
+def display_scan(scan, classes=""):
+    return {
+        'scan': scan,
+        'classes': classes
+    }
