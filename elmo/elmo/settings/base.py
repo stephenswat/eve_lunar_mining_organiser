eb2898a2ee28b21a8e734702d2f4eeae2c4e5244
Stephen Swatman <stephenswat@gmail.com>
2017-10-23 13:21:12 +0200

    Disable I18N and L10N.

--- elmo/elmo/settings/base.py
+++ elmo/elmo/settings/base.py
@@ -127,13 +127,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
+USE_I18N = False
+USE_L10N = False
 
 USE_TZ = True
+TIME_ZONE = 'UTC'
 
 
 # Static files (CSS, JavaScript, Images)
